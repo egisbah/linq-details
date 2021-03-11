32072f2a88d9e0e7a223a9084b14e41af46a209f
Tadas Kinderis <tadas.kinderis@teliacompany.com>
2021-03-11 09:32:06 +0200

    Add project files.

---
+++ LinqDetails/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDetails
+{
+    class Program
+    {
+        static void Main()
+        {
+            var source = new List<string>() { "hi", "lol", "insanguination", "bye" , "Nebuchadnezzar" };
+
+            var linqResult = source
+                .Where(n => n.Length > 10)
+                .Select(n => n.ToUpper());
+
+            var listFilter = MoreThanFive(source);
+            var listMap = AddTen(listFilter);
+
+            source.Add("defenestration");
+
+            Print(linqResult);
+            Print(listMap);
+        }
+
+        private static IEnumerable<string> MoreThanFive(IEnumerable<string> collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<string> AddTen(IEnumerable<string> collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Print(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
